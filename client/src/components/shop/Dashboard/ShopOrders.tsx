35313b8ee8d58047fa67345c20dec611dcf4c0ec
just9krish <rvamit2648@gmail.com>
2023-06-10 16:17:34 +0530

    now seller can see his all orders

--- client/src/components/shop/Dashboard/ShopOrders.tsx
+++ client/src/components/shop/Dashboard/ShopOrders.tsx
@@ -1,97 +1,81 @@
-import { GridCellParams } from "@mui/x-data-grid";
+import { ClassNamesProps } from "@emotion/react";
+import { DataGrid, GridCellParams } from "@mui/x-data-grid";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { formattedPrice } from "../../../helper/formatPrice";
 import { useAppSelector } from "../../../hooks";
 import { server } from "../../../server";
+import Loader from "../../Loader/Loader";
 
 export default function ShopOrders() {
-  const [ShopOrders, setShopOrders] = useState();
+  const [ShopOrders, setShopOrders] = useState([]);
   const { seller } = useAppSelector((state) => state.seller);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getShopOrders() {
+    setIsLoading(true);
+
     const { data } = await axios.get(`${server}/shops/${seller._id}/orders`, {
       withCredentials: true,
     });
 
     setShopOrders(data.orders);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     getShopOrders();
-    console.log(ShopOrders);
-    return () => {};
   }, []);
 
   const columns = [
     {
       field: "id",
-      headerName: "Product Id",
+      headerName: "Order ID",
       minWidth: 150,
       flex: 0.7,
-      maxWidth: 250,
+    },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params: GridCellParams) => {
+        return params.value == "Delivered" ? "greenColor" : "redcolor";
+      },
     },
     {
-      field: "name",
-      headerName: "Name",
-      minWidth: 180,
-      flex: 1.4,
-      maxWidth: 350,
-    },
-    {
-      field: "price",
-      headerName: "Price",
-      minWidth: 80,
-      flex: 0.6,
-      maxWidth: 120,
-    },
-    {
-      field: "discountpercentage",
-      headerName: "Discount Percentage",
-      minWidth: 80,
-      flex: 0.6,
-      maxWidth: 100,
-    },
-    {
-      field: "discountprice",
-      headerName: "Discount Price",
-      minWidth: 80,
-      flex: 0.6,
-      maxWidth: 120,
-    },
-    {
-      field: "stock",
-      headerName: "Stock",
+      field: "itemsQty",
+      headerName: "Items Qty",
       type: "number",
-      minWidth: 80,
-      flex: 0.6,
-      maxWidth: 120,
+      minWidth: 130,
+      flex: 0.7,
     },
+
     {
-      field: "sold",
-      headerName: "Sold out",
+      field: "total",
+      headerName: "Total",
       type: "number",
-      minWidth: 100,
-      flex: 0.6,
-      maxWidth: 150,
-    },
-    {
-      field: "Preview",
+      minWidth: 130,
       flex: 0.8,
-      minWidth: 100,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
       headerName: "",
       type: "number",
       sortable: false,
-      maxWidth: 100,
+
       renderCell: (params: GridCellParams) => {
-        const d = params.row.name;
-        const product_name = d.replace(/\s+/g, "-");
         return (
           <>
-            <Link to={`/products/${product_name}`}>
-              <button className="hover:bg-gray-200 bg-transparent rounded py-1.5 px-4 transition-all">
-                <AiOutlineEye size={20} />
+            <Link to={`/order/${params.id}`}>
+              <button>
+                <AiOutlineArrowRight size={20} />
               </button>
             </Link>
           </>
@@ -99,5 +83,27 @@ export default function ShopOrders() {
       },
     },
   ];
-  return <div></div>;
+
+  const row: any = [];
+
+  ShopOrders.forEach((order: any) => {
+    row.push({
+      id: order._id,
+      itemsQty: order.cart.length,
+      total: formattedPrice(order.totalPrice),
+      status: order.orderStatus,
+    });
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-[100%] lg:mx-8 pt-1 lg:mt-10 bg-white overflow-x-scroll">
+          <DataGrid rows={row} columns={columns} autoHeight />
+        </div>
+      )}
+    </>
+  );
 }
